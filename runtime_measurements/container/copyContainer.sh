5cc513d9a1d575788b6dfb0ddd587957df48551a
Raphael Dunkel <rsd6170+github@mailbox.org>
2024-12-01 19:40:18 +0100

    fix: fix path in copyHelper

--- runtime_measurements/container/copyContainer.sh
+++ runtime_measurements/container/copyContainer.sh
@@ -3,4 +3,4 @@
 rm -rf $HOME/fe4femo/runtime_measurements/container/enroot/
 rm -rf $HOME/.local/share/enroot/pyxis_*
 
-scp -i ~/.ssh/bwCloud_private -r ubuntu@134.60.155.236:/home/ubuntu/MA_docker/enroot $HOME/fe4femo/runtime_measurements/container/
+scp -i ~/.ssh/bwCloud_private -r ubuntu@134.60.155.236:/home/ubuntu/MA/MA_docker/enroot $HOME/fe4femo/runtime_measurements/container/
