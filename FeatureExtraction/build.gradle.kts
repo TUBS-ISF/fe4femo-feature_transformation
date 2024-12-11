94886ba49a11bea5c08e12bfc97ae9fb876d2e63
Raphael Dunkel <rsd6170+github@mailbox.org>
2024-12-11 19:35:54 +0000

    feat: change gradle version handling

--- FeatureExtraction/build.gradle.kts
+++ FeatureExtraction/build.gradle.kts
@@ -9,14 +9,17 @@ repositories {
     mavenCentral()
 }
 
+val log4japi = "2.24.2"
+val log4jcore = "2.24.2"
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-api
-    implementation("org.apache.logging.log4j:log4j-api:2.24.2")
+    implementation("org.apache.logging.log4j:log4j-api:$log4japi")
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
-    runtimeOnly("org.apache.logging.log4j:log4j-core:2.24.2")
+    runtimeOnly("org.apache.logging.log4j:log4j-core:$log4jcore")
     runtimeOnly("org.apache.logging.log4j:log4j-layout-template-json")
 
 }
