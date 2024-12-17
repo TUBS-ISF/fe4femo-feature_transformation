22c4d242d6765ed962c8fcc99d351b8c87bb964c
Raphael Dunkel <rsd6170+github@mailbox.org>
2024-12-17 21:00:36 +0000

    fix: fix bug in cyclic dependency metrics

--- FeatureExtraction/src/main/java/de/uniulm/sp/fe4femo/featureextraction/analyses/dymmer/DyMMerHelper.java
+++ FeatureExtraction/src/main/java/de/uniulm/sp/fe4femo/featureextraction/analyses/dymmer/DyMMerHelper.java
@@ -277,7 +277,7 @@ public class DyMMerHelper {
         checkInstance(fmInstance);
         if (singleCyclicDependentFeatures < 0){
             if (constrainedFeatures == null) computeConstrainedFeatures(fmInstance.featureModel());
-            singleCyclicDependentFeatures = fmInstance.featureModel().getFeatures().stream()
+            singleCyclicDependentFeatures = constrainedFeatures.stream()
                     .filter(e -> e.getStructure().isAlternative() || (e.getStructure().getParent() != null && e.getStructure().getParent().isAlternative()))
                     .count();
         }
@@ -287,7 +287,7 @@ public class DyMMerHelper {
         checkInstance(fmInstance);
         if (multiCyclicDependentFeatures < 0){
             if (constrainedFeatures == null) computeConstrainedFeatures(fmInstance.featureModel());
-            multiCyclicDependentFeatures = fmInstance.featureModel().getFeatures().stream()
+            multiCyclicDependentFeatures = constrainedFeatures.stream()
                     .filter(e -> e.getStructure().isOr() || (e.getStructure().getParent() != null && e.getStructure().getParent().isOr()))
                     .count();
         }
