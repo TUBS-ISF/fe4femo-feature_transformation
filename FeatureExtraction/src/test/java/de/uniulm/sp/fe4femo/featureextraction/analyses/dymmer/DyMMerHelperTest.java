416599130fd95ff62a6075e950492ea2f1cd2151
Raphael Dunkel <rsd6170+github@mailbox.org>
2024-12-19 08:39:43 +0000

    feat: add empty testing framework for dymmer

---
+++ FeatureExtraction/src/test/java/de/uniulm/sp/fe4femo/featureextraction/analyses/dymmer/DyMMerHelperTest.java
@@ -0,0 +1,140 @@
+package de.uniulm.sp.fe4femo.featureextraction.analyses.dymmer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DyMMerHelperTest {
+
+    private DyMMerHelper dymmerHelper;
+
+    @BeforeEach
+    void setUp() {
+        dymmerHelper = new DyMMerHelper();
+    }
+
+    @Test
+    void getNoMandatoryFeatures() {
+    }
+
+    @Test
+    void getNoDeadFeatures() {
+    }
+
+    @Test
+    void getFeatureCount() {
+    }
+
+    @Test
+    void getTopFeatures() {
+    }
+
+    @Test
+    void getLeafFeatures() {
+    }
+
+    @Test
+    void getMaxDepth() {
+    }
+
+    @Test
+    void getMedianDepth() {
+    }
+
+    @Test
+    void getMeanDepth() {
+    }
+
+    @Test
+    void getOrGroups() {
+    }
+
+    @Test
+    void getXorGroups() {
+    }
+
+    @Test
+    void getBranchingFactorMedian() {
+    }
+
+    @Test
+    void getBranchingFactorMean() {
+    }
+
+    @Test
+    void getBranchingFactorMax() {
+    }
+
+    @Test
+    void getRootCount() {
+    }
+
+    @Test
+    void getSingleCyclicDependentFeatures() {
+    }
+
+    @Test
+    void getMultiCyclicDependentFeatures() {
+    }
+
+    @Test
+    void getChildCountXor() {
+    }
+
+    @Test
+    void getChildCountOr() {
+    }
+
+    @Test
+    void getGroupedXor() {
+    }
+
+    @Test
+    void getGroupedOr() {
+    }
+
+    @Test
+    void getOptionalFeatures() {
+    }
+
+    @Test
+    void getConstraintCount() {
+    }
+
+    @Test
+    void getCompoundComplexity() {
+    }
+
+    @Test
+    void getConnectivityDensityCoefficent() {
+    }
+
+    @Test
+    void getCountGrouped() {
+    }
+
+    @Test
+    void getCountGroups() {
+    }
+
+    @Test
+    void getVariabilityRatio() {
+    }
+
+    @Test
+    void getNoFeaturesConstraintRefExceptParents() {
+    }
+
+    @Test
+    void getMeanRefdFeaturesInConstraintsExceptParentPerFeature() {
+    }
+
+    @Test
+    void getNoFeaturesWithChildren() {
+    }
+
+    @Test
+    void getNoConstrainedFeatures() {
+    }
+}
