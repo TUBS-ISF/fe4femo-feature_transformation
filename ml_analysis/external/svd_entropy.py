654a0077ec0ffa14fb30626c60d90680309c23ad
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-01-03 19:59:11 +0000

    feat: add svd_entropy selection

---
+++ ml_analysis/external/svd_entropy.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from scipy.linalg import svdvals
+
+
+def compute_dataset_entropy(matrix : np.ndarray) -> float:
+    svdval = svdvals(matrix)
+    rank = np.linalg.matrix_rank(matrix)
+    eigenvalues = svdval * svdval
+    cumulator = 0
+    eigenvalues_sum = sum(eigenvalues)
+    for eigenvalue in eigenvalues:
+        vj = eigenvalue / eigenvalues_sum
+        cumulator += vj * np.log2(vj)
+    return -1 * (1/ np.log2(rank)) * cumulator
+
+def compute_feature_contribution(matrix : np.ndarray, position : int, full_entropy : float = None) -> float:
+    if full_entropy is None:
+        full_entropy = compute_dataset_entropy(matrix)
+    partial_entropy = compute_dataset_entropy(np.delete(matrix, position, 1))
+    return full_entropy - partial_entropy
+
+def keep_high_contrib_features(df : pd.DataFrame) -> pd.DataFrame:
+    np_view = df.to_numpy()
+    full_entropy = compute_dataset_entropy(np_view)
+    contributions = np.array([compute_feature_contribution(np_view, i, full_entropy) for i in range(np_view.shape[1])])
+    min_acceptance = contributions.mean() + contributions.std()
+    bool_mask = [ x > min_acceptance for x in contributions]
+    return df.loc[:, bool_mask]
+
