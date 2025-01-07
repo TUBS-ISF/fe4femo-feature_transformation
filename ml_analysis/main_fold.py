3f4cd92646f4db9123646685a97f4364d112c4ad
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-01-07 10:16:49 +0000

    feat: add starter helper for cross-val

---
+++ ml_analysis/main_fold.py
@@ -0,0 +1,27 @@
+import argparse
+import subprocess
+import sys
+
+from helper.input_parser import parse_input
+
+
+#
+# 1. 10-CV
+#     - Methode für get-HPO (Model + Selection --> Optuna + static-fold Cross-Val) auf 9-split
+#     - Preproc + Trainieren Model auf 9-split mit HPs --> ggf. mit frozen_trial --> alle relevanten Metriken speichern
+# 3. Modell mit selbigen wie in CV trainieren auf ganzen Train --> Speichern + HPs
+# 4. weitere Eval auf Model --> Speichern
+
+# als separate Main: Multi-Objective für RQ2b
+
+def main(pathOutput : str):
+    for i in range(10):
+        arguments = ["sbatch", "--partition=multiple", f"--output={pathOutput}/{i}.out", "slurm_scripts/run.sh", "--foldNo", f"{i}" ]
+
+        arguments.extend(sys.argv[1:])
+        subprocess.run(arguments, shell=True)
+
+
+if __name__ == '__main__':
+    args = parse_input()
+    main(args.pathOutput)
