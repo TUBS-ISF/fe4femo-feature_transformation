c1e4eba81870d66d85b280c524eb7f3bbca3bf4a
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-01-07 16:44:15 +0000

    feat: handle env vars in inputpaths

--- ml_analysis/generate_fold_model.py
+++ ml_analysis/generate_fold_model.py
@@ -158,4 +158,4 @@ def main(pathData: str, pathOutput: str, features: str, task: str, model: str, m
 if __name__ == '__main__':
     args = parse_input()
 
-    main(args.pathData, args.pathOutput, args.features, args.task, args.model, args.modelHPO, args.HPOits, args.foldNo)
+    main(os.path.expandvars(args.pathData), os.path.expandvars(args.pathOutput), args.features, args.task, args.model, args.modelHPO, args.HPOits, args.foldNo)
