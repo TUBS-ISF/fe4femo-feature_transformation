d6b284c7c9ec1cbf8393124e45137d8eb42b4e5b
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-01-08 15:24:46 +0000

    feat: try different dask configuration

--- ml_analysis/slurm_scripts/run.sh
+++ ml_analysis/slurm_scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=2:0:0
+#SBATCH --time=3:0:0
 #SBATCH --job-name=eval_model
-#SBATCH --ntasks=100
-#SBATCH --cpus-per-task=6
+#SBATCH --ntasks=80
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1125
 #SBATCH --nodes=2-70
 #SBATCH --use-min-nodes
