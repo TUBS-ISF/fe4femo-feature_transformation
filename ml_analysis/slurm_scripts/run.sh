1669099e5800c1c9f170b79dd5f0af8c3150f74a
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-01-08 09:03:42 +0000

    feat: reduce slurm constraints for testing

--- ml_analysis/slurm_scripts/run.sh
+++ ml_analysis/slurm_scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=0:30:0
 #SBATCH --job-name=eval_model
-#SBATCH --ntasks=10
+#SBATCH --ntasks=20
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=1125
 #SBATCH --nodes=2,70
