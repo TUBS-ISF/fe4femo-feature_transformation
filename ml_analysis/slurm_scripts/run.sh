f7202eb85371190d09e7ab224048d6b73310265a
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-01-22 18:22:58 +0000

    feat: supress future warnings caused by intel sklearn

--- ml_analysis/slurm_scripts/run.sh
+++ ml_analysis/slurm_scripts/run.sh
@@ -25,5 +25,5 @@ ENROOT_CONFIG_PATH=$HOME/enroot_config/
 # helper: srun --container-image=$HOME/fe4femo/ml_analysis/slurm_scripts/ml_analysis_i.sqsh --container-name=ml_analysis:no_exec    --container-mounts=/etc/slurm/task_prolog:/etc/slurm/task_prolog,/scratch:/scratch    --container-workdir=/app/ --time=10 --partition=dev_single --no-container-entrypoint /bin/bash
 srun --exact --container-image=ghcr.io#rsd6170/ml_analysis:0.1 --container-name=ml_analysis:no_exec \
    --container-mounts=/etc/slurm/task_prolog:/etc/slurm/task_prolog,/scratch:/scratch  --container-mount-home \
-   --container-workdir=/app/ --no-container-entrypoint conda run --no-capture-output -n ml_analysis python generate_fold_model.py $@
+   --container-workdir=/app/ --no-container-entrypoint conda run --no-capture-output -n ml_analysis python -Wignore::FutureWarning generate_fold_model.py $@
 
