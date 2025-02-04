fb3cf2c96131eefcdb8ae0a3e4e399c2caa50263
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-02-04 15:27:53 +0000

    feat: add submission skript

---
+++ ml_analysis/slurm_scripts/starter.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+config=$HOME/fe4femo/ml_analysis/slurm_scripts/config.txt
+output_path="fe4femo/ml_analysis/out/"
+script_path="$HOME/fe4femo/ml_analysis/slurm_scripts/run.sh"
+data_path="raphael-dunkel-master/data/"
+
+
+partition="${1:-multiple_il}"
+maxIt=$(wc -l < $inputpath)
+maxConcurrent=50
+
+tmpQUEUE=$(squeue | grep -c '^')
+waitIt=$((maxConcurrent + 1 - tmpQUEUE))
+
+while [[ maxIt -ge 1 ]] ; do
+    while [[ $waitIt -le "-20" ]]
+    do
+        sleep 30
+        queue=$(squeue | grep -c '^')
+        free=$((maxConcurrent + 1 - queue))
+        echo "$free free spaces in queue"
+        waitIt=$free
+    done
+    ((waitIt=waitIt-1))
+    echo "model $maxIt submitted"
+    EXPERIMENT_NO=$maxIt
+
+    # parameters
+
+    name=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $2}' $config)
+    task_count=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $3}' $config)
+    runtime=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $4}' $config)
+    out_path=$HOME/${output_path}/${name}.out
+    fold_no=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $5}' $config)
+    feature=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $6}' $config)
+    task=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $7}' $config)
+    model=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $8}' $config)
+    HPOits=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $9}' $config)
+    bool_modelHPO=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $10}' $config)
+    [[ "$bool_modelHPO" == "True" ]] && modelHPO="--modelHPO" || modelHPO=""
+    bool_selectorHPO=$(awk -v ID=$EXPERIMENT_NO '$1==ID {print $11}' $config)
+    [[ "$bool_selectorHPO" == "True" ]] && selectorHPO="--selectorHPO" || selectorHPO=""
+
+    ############
+
+    output=$(sbatch -J "$name" -n "$task_count" --time="$runtime" --output=$out_path  --partition="${partition}" "$script_path" --foldNo "$fold_no" --feature "$feature" --task "$task" --model "$model" --HPOits "$HPOits" "$modelHPO" "$selectorHPO" "$data_path" "$output_path" )
+    if [[ $output =~ "error" ]]; then
+       echo "Error in Submission, trying again!"
+    else
+       ((maxIt=maxIt-1))
+    fi
+done
+
+mail -s "Finished Job Submission \"${2}\"" "raphael.dunkel@uni-ulm.de" <<< "Submission script finished, but jobs could still be running. Check with command 'squeue' to list all currently running jobs!"
