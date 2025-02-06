f247028c4ff9017aba7ab8716d06328edf22f74e
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-02-06 21:21:25 +0000

    feat: add new times to config creator

--- ml_analysis/config_creator.py
+++ ml_analysis/config_creator.py
@@ -1,4 +1,6 @@
 import itertools
+import math
+from datetime import timedelta
 
 from generate_fold_model import create_run_name
 from helper.input_parser import get_feature_list, get_model_list, get_task_list
@@ -13,11 +15,35 @@ def get_task_count(feature: str) -> int:
     if feature in ["genetic"]:
         return 256
     else:
-        return 256
+        return 192
 
-def get_runtime() -> str:
-    #todo add times
-    pass
+def get_runtime(hpoIts: int, feature: str) -> str:
+    selector_modifier_m = {
+        "all" : 30,
+        "prefilter" : 10,
+        "SATzilla" : 5,
+        "SATfeatPy" : 8,
+        "FMBA" : 5,
+        "FM_Chara" : 4,
+        "kbest-mutalinfo" : 7,
+        "multisurf" : 8,
+        "mRMR" : 6,
+        "RFE" : 45,
+        "genetic" : 90,
+        "HFMOEA" : 120,
+        "embedded-tree" : 10,
+        "SVD-entropy" : 4,
+        "NDFS" : 8,
+        "optuna-combined" : 6
+    }
+
+    final_modifier = 1.2
+    value = selector_modifier_m[feature]
+    if feature != "genetic":
+        value *= hpoIts / 50
+    value *= final_modifier
+
+    return str(math.ceil(value))
 
 def check_valid(feature: str, model: str, task: str) -> bool:
     if feature == "harris-hawks":
@@ -26,17 +52,30 @@ def check_valid(feature: str, model: str, task: str) -> bool:
         return False
     return True
 
-hpoIts=200
-combinations = [ (fold, feature, model, task) for fold, feature, model, task in itertools.product(range(10), get_feature_list(), get_model_list(), get_task_list()) ]
-
-experiments = [
-    f"{i} {create_run_name(feature, task, model, is_modelHPO(feature), is_selectorHPO(feature), hpoIts, fold)} {get_task_count(feature)} {get_runtime()} {fold} {feature} {task} {model} {hpoIts} {is_modelHPO(feature)} {is_selectorHPO(feature)}"
-    for i, (fold, feature, model, task) in enumerate(combinations) if check_valid(feature, model, task)
-]
+def check_desired(feature: str, model: str, task: str):
+    return True
 
 
+if __name__ == '__main__':
+    hpoIts=150
+    combinations = [
+        (fold, feature, model, task) for fold, feature, model, task in itertools.product(range(10), get_feature_list(), get_model_list(), get_task_list())
+        if check_valid(feature, model, task) and check_desired(feature, model, task)
+    ]
 
-with open("config.txt", "w") as f:
-    f.write("NO NAME TASK_COUNT RUNTIME FOLD_NO FEATURE TASK MODEL HPO_ITS MODEL_HPO SELECTOR_HPO\n")
-    f.write("\n".join(experiments))
+    experiments = [
+        f"{i} {create_run_name(feature, task, model, is_modelHPO(feature), is_selectorHPO(feature), hpoIts, fold)} {get_task_count(feature)} {get_runtime(hpoIts, feature)} {fold} {feature} {task} {model} {hpoIts} {is_modelHPO(feature)} {is_selectorHPO(feature)}"
+        for i, (fold, feature, model, task) in enumerate(combinations)
+    ]
+
+    runtime_estimation = 0
+    for experiment in experiments:
+        runtime_estimation += int(experiment.split(" ")[3])
+    print(f"Total estimated sequential runtime: {timedelta(minutes=runtime_estimation)}")
+
+
+
+    with open("config.txt", "w") as f:
+        f.write("NO NAME TASK_COUNT RUNTIME FOLD_NO FEATURE TASK MODEL HPO_ITS MODEL_HPO SELECTOR_HPO\n")
+        f.write("\n".join(experiments))
 
