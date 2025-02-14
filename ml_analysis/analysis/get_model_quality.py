2f0ca6ed9fda9cd5af5468dfffc389a921f9b4af
Raphael Dunkel <rsd6170+github@mailbox.org>
2025-02-14 22:02:01 +0000

    feat: update model quality checker to new pickle standard

--- ml_analysis/analysis/get_model_quality.py
+++ ml_analysis/analysis/get_model_quality.py
@@ -7,13 +7,13 @@ def get_model_quality(file):
     dictonary = get_pickle_dict(file)
     ret_list = []
     for trial_container in dictonary["trial_container"]:
-
-        y_pred = trial_container.model.predict(dictonary["X_test"])
+        y_pred = trial_container.model.predict(trial_container.x_test)
         y_test = dictonary["y_test"]
         if is_classifier(trial_container.model):
-            ret_list.append(matthews_corrcoef(y_test, y_pred))
+            ret_list.append(matthews_corrcoef(y_test, y_pred).item())
         else:
-            ret_list.append(d2_absolute_error_score(y_test, y_pred))
+            ret_list.append(d2_absolute_error_score(y_test, y_pred).item())
+    return ret_list
 
 if __name__ == '__main__':
     path = "/home/ubuntu/MA/fe4femo/ml_analysis/copy/"
